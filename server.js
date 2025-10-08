fab001a58f17eafeccaf473a3e27abe198a5820a
ANAGHA-A-04 <anagha8848@gmail.com>
2025-10-09 00:24:35 +0530

    added the router

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 import connect from "./mongo/db.js";
 connect();
+import router from "./routes/authroute.js";
+
 
 const app=express();
 app.use(express.json());
 app.get("/",(req,res)=>{
     res.send("api is runnning");
 });
+app.use("/api/user",router)
 const PORT = process.env.PORT||5000;
 app.listen(PORT,()=>{
     console.log("hi");
